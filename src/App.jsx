5e9352051ce9a06073d79eedd64421edce0a196b
Devashish Singh Parmar <parmardevashish132@gmail.com>
2023-12-03 20:14:12 +0530

    Using hooks better.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import ColorBar from "./ColorBar"
+import { useState } from "react";
 
 function App() {
-
+  const [colors, setColor] = useState("salmon");
+  // function changeColor() {
+  //     setColor(colors = props.color)
+  //     console.log("hook "+ colors +"\n clicked "+props.color)
+  //     document.getElementById("fullScreen").style.backgroundColor = colors;
+  // }
 
   return (
     <>
-      <div className="bg-white p-3 h-screen items-end" id="fullScreen" style={{ backgroundColor: "salmon" }}>
+      <div className="p-3 h-screen items-end" id="fullScreen" style={{ backgroundColor: colors }}>
         <div className=" flex flex-wrap justify-center fixed bottom-5 w-screen">
-          <ColorBar name="Green" color="green" />
-          <ColorBar name="Red" color="red" />
-          <ColorBar name="Blue" color="blue" />
-          <ColorBar name="Olive" color="olive" />
-          <ColorBar name="Gray" color="gray" />
-          <ColorBar name="Yellow" color="yellow" />
-          <ColorBar name="Pink" color="pink" />
-          <ColorBar name="Purple" color="purple" />
-          <ColorBar name="Lavender" color="lavender" />
-          <ColorBar name="Black" color="black" />
-          <ColorBar name="White" color="white" />
+          <ColorBar name="Green" color="green" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Red" color="red" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Blue" color="blue" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Olive" color="olive" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Gray" color="gray" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Yellow" color="yellow" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Pink" color="pink" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Purple" color="purple" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Lavender" color="lavender" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="Black" color="black" onclick={(color) => {
+            setColor(color)
+          }} />
+          <ColorBar name="White" color="white" onclick={(color) => {
+            setColor(color)
+          }} />
         </div>
       </div>
     </>
--- src/ColorBar.jsx
+++ src/ColorBar.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
 function ColorBar(props) {
-    let [colors, setColor] = useState("salmon");
-    function changeColor() {
-        setColor(colors = props.color)
-        console.log("hook "+ colors +"\n clicked "+props.color)
-        document.getElementById("fullScreen").style.backgroundColor = colors;
-    }
+    // let [colors, setColor] = useState("salmon");
+    // function changeColor() {
+    //     setColor(colors = props.color)
+    //     console.log("hook "+ colors +"\n clicked "+props.color)
+    //     document.getElementById("fullScreen").style.backgroundColor = colors;
+    // }
     return (
         <button
             className={`px-4 py-3 m-1 rounded-3xl shadow-lg`}
             style={{ backgroundColor: `${props.color}`, color: `${props.color === "black" ? "white" : "black"}` }}
-            onClick={changeColor}
+            onClick={()=>{
+                props.onclick(props.color)
+                }}
         >
             {props.name}
         </button>  
